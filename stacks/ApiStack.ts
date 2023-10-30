a2c934211ebf12f7912aadbd7d92c034320ac132
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-10-30 12:24:34 -0700

    Securing the API

---
+++ stacks/ApiStack.ts
@@ -0,0 +1,27 @@
+import { Api, StackContext, use } from "sst/constructs";
+import { StorageStack } from "./StorageStack";
+export function ApiStack({ stack }: StackContext) {
+  const { table } = use(StorageStack);
+  // Create the API
+  const api = new Api(stack, "Api", {
+    defaults: {
+      authorizer: "iam",
+      function: {
+        bind: [table],
+      },
+    },
+    routes: {
+      "DELETE /notes/{id}": "packages/functions/src/delete.main",
+      "PUT /notes/{id}": "packages/functions/src/update.main",
+      "GET /notes": "packages/functions/src/list.main",
+      "GET /notes/{id}": "packages/functions/src/get.main",
+      "POST /notes": "packages/functions/src/create.main",
+    },
+  });
+  // Show the API endpoint in the output
+  stack.addOutputs({ ApiEndpoint: api.url });
+  // Return the API resource
+  return {
+    api,
+  };
+}
